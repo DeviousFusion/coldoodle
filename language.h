875cedc9f6dda7103bd956d7b5565038beb9f8e7
Tushar Saini <dj3tusk@gmail.com>
2015-07-29 18:46:58 -0500

    String fixes

--- language.h
+++ language.h
@@ -25,7 +25,7 @@
 #define PROTOCOL_VERSION "1.0"
 #define FIRMWARE_URL "http://www.solidoodle.com/how-to-2/how-to-update-firmware/"
 
-#define MACHINE_NAME "Solidoodle_3"
+#define MACHINE_NAME "Solidoodle_3-Cold_Variant"
 
 #if MOTHERBOARD == 62
 	#define SOLI_BOARD "Sanguinololu"
@@ -150,7 +150,7 @@
 	#define MSG_HEATING_COMPLETE "Heating done."
 	#define MSG_BED_HEATING "Bed Heating."
 	#define MSG_BED_DONE "Bed done."
-	#define MSG_M115_REPORT "COLDOODLE_FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for " SOLI_BOARD " FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) "\n"
+	#define MSG_M115_REPORT "FIRMWARE_NAME:Marlin V1; Sprinter/grbl mashup for " SOLI_BOARD " FIRMWARE_URL:" FIRMWARE_URL " PROTOCOL_VERSION:" PROTOCOL_VERSION " MACHINE_TYPE:" MACHINE_NAME " EXTRUDER_COUNT:" STRINGIFY(EXTRUDERS) "\n"
 	#define MSG_COUNT_X " Count X: "
 	#define MSG_ERR_KILLED "Printer halted. kill() called!"
 	#define MSG_ERR_STOPPED "Printer stopped due to errors. Fix the error and use M999 to restart. (Temperature is reset. Set it after restarting)"
